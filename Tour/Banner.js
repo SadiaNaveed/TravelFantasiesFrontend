159514f5b3358aca74bb8e3214b4b6ccc81e00b3
Asma <56886888+ASMA-WEB@users.noreply.github.com>
2020-11-27 11:48:59 +0500

    Add files via upload

---
+++ Tour/Banner.js
@@ -0,0 +1,66 @@
+import React from "react";
+// import Link from "@material-ui/core/Link";
+import "./Banner.css";
+import { Link } from "react-router-dom";
+import { Button, Col } from "reactstrap";
+import { Animate } from "react-simple-animate";
+
+function Banner() {
+  return (
+    <header id="showcase">
+      <div class="showcase-content">
+        <Animate
+          play={true} // set play true to start the animation
+          duration={1} // how long is the animation duration
+          delay={0.3} // how many delay seconds will apply before the animation start
+          start={{ transform: "translate(0, -500px)" }}
+          end={{ transform: "translate(0px, 0px)" }}
+          // complete={{ display: "none" }}
+          easeType="cubic-bezier(0.445, 0.05, 0.55, 0.95)"
+          // onComplete={onCompleteCallBack} // call back function when animation is completed
+        >
+          <h1 style={{ fontSize: "75px" }} class="l-heading">
+            <span style={{ color: "#2E6E7B", fontFamily: "revert" }}>
+              Travel{" "}
+            </span>
+            <span style={{ color: "#56DDF8", fontFamily: "revert" }}>
+              Fantasies
+            </span>
+          </h1>
+        </Animate>
+        <Animate
+          play={true} // set play true to start the animation
+          duration={1} // how long is the animation duration
+          delay={0.3} // how many delay seconds will apply before the animation start
+          start={{ transform: "translate(-500px, 0px)" }}
+          end={{ transform: "translate(0px, 0px)" }}
+          // complete={{ display: "none" }}
+          easeType="cubic-bezier(0.445, 0.05, 0.55, 0.95)"
+          // onComplete={onCompleteCallBack} // call back function when animation is completed
+        >
+          <p style={{ fontSize: "20px" }}>A More Rewarding Way To Travel</p>
+        </Animate>
+        <Animate
+          play={true} // set play true to start the animation
+          duration={1} // how long is the animation duration
+          delay={0.5} // how many delay seconds will apply before the animation start
+          start={{ transform: "translate(0px, 500px)" }}
+          end={{ transform: "translate(0px, 0px)" }}
+          // complete={{ display: "none" }}
+          easeType="cubic-bezier(0.445, 0.05, 0.55, 0.95)"
+          // onComplete={onCompleteCallBack} // call back function when animation is completed
+        >
+          <Link to={"/Become_a_host"} style={{ textDecoration: "none" }}>
+            <Col col="3" md="12">
+              <Button style={{ marginTop: 50 }} outline color="info">
+                Become a Host
+              </Button>
+            </Col>
+          </Link>
+        </Animate>
+      </div>
+    </header>
+  );
+}
+
+export default Banner;
