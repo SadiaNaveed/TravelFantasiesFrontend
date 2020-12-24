4ab824ebf06ed8974431f73932bba89e932c2422
Asma <56886888+ASMA-WEB@users.noreply.github.com>
2020-12-24 17:19:24 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ import Footer from "./components/Footer";
 import LandingPage from "./components/LandingPage";
 import ContactUs from "./components/Contactus";
 import NotFound from "./components/NotFound";
-import Login from "./components/Login";
-import Blog from "./components/Blog";
+//import Login from "./components/Login";
+import Blog from "./components/Blogs/Blog";
 //import Signup from "./components/Signup";
 import Product from "./components/products";
 import Hotels from "./components/Hotels/Hotels";
@@ -22,9 +22,9 @@ import addHotel from "./components/Admin/fragments/AddHotel";
 import Aboutus from "./components/Aboutus";
 import Itinerary from "./components/Itinerary";
 import Compare from "./components/Hotels/compare";
-import Dashboard from "./components/Admin/Dashboard"
-import UserDashboard from "./components/User/Dashboard"
-import GuideDashboard from "./components/Guide/Dashboard"
+import Dashboard from "./components/Admin/Dashboard";
+import UserDashboard from "./components/User/Dashboard";
+import GuideDashboard from "./components/Guide/Dashboard";
 import HotelBooking from "./components/Hotels/HotelBooking";
 import HotelDetail from "./components/Hotels/HotelDetail";
 import HotelView from "./components/Admin/fragments/HotelView";
@@ -33,6 +33,21 @@ import AllRooms from "./components/Admin/fragments/AllRooms";
 import placeDetail from "./components/Places/PlacesToVisit";
 import RoomView from "./components/Admin/fragments/RoomView";
 import RoomDetail from "./components/Hotels/RoomDetail";
+import Tours from "./components/Tour/Tours";
+import Become_a_host from "./components/Host/Become_a_host";
+import BecomeGuide from "./components/Guide-page/BecomeGuide";
+import BookTour from "./components/Bookings/BookTour";
+import Packages from "./components/Packages/Packages";
+import TourDetailPage from "./components/Tour/TourDetailPage";
+import Guide from "./components/Guide-page/Guide";
+import Signup from "./components/auth/signupComponent";
+import Login from "./components/auth/loginComponent";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import MapView from "./components/Hotels/MapView";
+import Payment from "./components/Hotels/Payment";
+import ProtectedRoute from "./components/ProctedRoute";
+import userService from './services/UserService';
 // const handlAddToCartClick = (title) => {
 //   alert("Add to cart clicked for " +title);
 // };
@@ -50,6 +65,7 @@ function App() {
     <Router>
       <div>
         {/* <h1 style = {{backgroundColor:"aqua"}}> Products Details </h1>  */}
+        <ToastContainer />
         <TopMenu />
         <Switch>
           <Route path="/products" component={Product} />
@@ -58,28 +74,41 @@ function App() {
           <Route path="/not-found" component={NotFound} />
           <Route path="/hotel/add" component={addHotel} />
           <Route path="/HotelDetail" component={HotelDetail} />
+          <Route path="/HotelMapView" component={MapView} />
           <Route path="/HotelBooking" component={HotelBooking} />
           <Route path="/hotels/:page/:perPage" component={Hotels} />
           <Route path="/login" component={Login} />
           <Route path="/Blog" exact component={Blog} />
-          <Route path="/PlacesToVisit" component={placeDetail} />      
-          <Route path="/hotel/compare" component={Compare} />      
-          <Route path="/AddRoom" component={AddRoom} />  
-          <Route path="/RoomsView" component={AllRooms} /> 
-          <Route path="/SingleRoomView" component={RoomView} /> 
-          <Route path="/RoomDetail" component={RoomDetail} /> 
-          <Route path="/HotelView" component={HotelView} />       
+          <Route path="/PlacesToVisit" component={placeDetail} />
+          <Route path="/hotel/compare" component={Compare} />
+          <Route path="/AddRoom" component={AddRoom} />
+          <Route path="/RoomsView" component={AllRooms} />
+          <Route path="/SingleRoomView" component={RoomView} />
+          <Route path="/RoomDetail" component={RoomDetail} />
+          <Route path="/HotelView" component={HotelView} />
           <Route path="/admin-dashboard" component={Dashboard} />
           <Route path="/user-dashboard" component={UserDashboard} />
           <Route path="/guide-dashboard" component={GuideDashboard} />
           <Route path="/HotelBooking/:id" component={HotelBooking} />
+          <Route path="/HotelPayment" component={Payment} />
           {/* <Route path="/sign-up" exact component={Signup} /> */}
           <Route path="/itinerary/:page/:perPage" component={Itinerary} />
           <Route path="/" exact component={LandingPage} />
+          <Route path="/Tours" component={Tours} />
+          <ProtectedRoute path="/Become_a_host" login={ userService.getLoggedInUser() != null  ? true : false } component={Become_a_host} />
+          <ProtectedRoute path="/BecomeGuide" login={ userService.getLoggedInUser() != null  ? true : false } component={BecomeGuide} />
+          <Route path="/book-tour" component={BookTour} />
+          <Route path="/travel-packages" component={Packages} />
+          <Route path="/TourDetailPage" component={TourDetailPage} />
+          <Route path="/Guide" component={Guide} />
+          <Route path="/sign-up" component={Signup} />
+          <Route path="/login" component={Login} />
           <Redirect to="not-found" />
         </Switch>
         <Footer />
-        {/* <Counter count={count} handleIncrement={handleIncrement} handleDecrement={handleDecrement} />
+{/*
+       
+         <Counter count={count} handleIncrement={handleIncrement} handleDecrement={handleDecrement} />
       <Product title="Audi"  price="900" onAdToCart = {handlAddToCartClick} count={count}/>
       <Product title="Suzuki" price="800" onAdToCart = {handlAddToCartClick} count={count}/>
       <Product title="Mehran" price="200" onAdToCart = {handlAddToCartClick} count={count}/> */}
